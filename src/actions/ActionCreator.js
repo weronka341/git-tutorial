23c73340de987fcbb4397c602b4b1e732bd11ae8
Weronika Szpytma <weronka341@gmail.com>
2019-11-24 19:21:06 +0100

    Update actions

--- src/actions/ActionCreator.js
+++ src/actions/ActionCreator.js
@@ -1,3 +1,5 @@
+import {actions, exerciseActions} from './Action';
+
 export const setVisibility = (action, isVisible) => ({
     type: action,
     isVisible,
@@ -6,4 +8,31 @@ export const setVisibility = (action, isVisible) => ({
 export const performActionOnSidebarOption = (action, optionID) => ({
     type: action,
     optionID,
+});
+
+export const resizeSvgContent = (action, shouldResizeContent) => ({
+    type: action,
+    shouldResizeContent,
+});
+
+export const addCommit = () => ({
+    type: actions.ADD_COMMIT,
+});
+
+export const refAction = (action, name) => ({
+    type: action,
+    name,
+});
+
+export const moveToExercise = (exerciseName) => ({
+    type: actions.MOVE_TO_EXERCISE,
+    exerciseName,
+});
+
+export const moveToContent = () => ({
+    type: actions.MOVE_TO_CONTENT,
+});
+
+export const checkExerciseStatus = () => ({
+    type: exerciseActions.CHECK_EXERCISE_STATUS,
 });
