5b539eb3c18bc6d997e4b7f5e8ee657da1536548
Weronika Szpytma <weronka341@gmail.com>
2019-12-04 20:35:34 +0100

    Delete resizing svg content and change svg position calculations

--- src/containers/svgDisplayArea/SVGDisplayArea.js
+++ src/containers/svgDisplayArea/SVGDisplayArea.js
@@ -2,29 +2,19 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {SVGCommitElement} from '../../components/svgElements/SVGCommitElement';
 import {SVGArrow} from '../../components/svgElements/SVGArrow';
-import {resizeSvgContent} from '../../actions/ActionCreator';
 import {SVGRefElement} from '../../components/svgElements/SVGRefElement';
-import {actions} from '../../actions/Action';
 import './SVGDisplayArea.css';
 
 class SVGDisplay extends React.Component {
-    componentDidUpdate() {
-        const parentRect = this.parent.getBoundingClientRect();
-        const childRect = this.child.getBoundingClientRect();
-        if (parentRect.width - this.props.dimensions.shift < childRect.width) {
-            this.props.resizeContent();
-        }
-    }
-
     renderCommits() {
         const {commits, dimensions} = this.props;
         return commits.map(commit =>
             <SVGCommitElement key={commit.index}
                               cx={this.calculateCommitXPosition(commit.index)}
-                              cy={this.calculateCommitYPosition(commit.index)}
                               hash={commit.hash}
                               positionY={dimensions.commit.positionY}
-                              offset={commit.offset}
+                              offset={this.calculateCommitYOffset(commit.index)}
+                              radius={dimensions.commit.radius}
             />
         );
     }
@@ -34,9 +24,9 @@ class SVGDisplay extends React.Component {
         return commits.filter(c => c.index).map((commit, index) =>
             <SVGArrow key={index}
                       x2={this.calculateCommitXPosition(commit.parentCommit)}
-                      y2={this.calculateCommitYPositionWithOffset(commit.parentCommit)}
+                      y2={this.calculateCommitYOffset(commit.parentCommit)}
                       x1={this.calculateCommitXPosition(commit.index)}
-                      y1={this.calculateCommitYPositionWithOffset(commit.index)}
+                      y1={this.calculateCommitYOffset(commit.index)}
                       positionY={dimensions.commit.positionY}
             />
         );
@@ -47,25 +37,21 @@ class SVGDisplay extends React.Component {
         return refs.map((ref, index) =>
             <SVGRefElement key={index}
                            x={this.calculateCommitXPosition(ref.commit)}
-                           y={this.calculateCommitYPosition(ref.commit)}
                            name={ref.name}
                            position={ref.position}
                            positionY={dimensions.commit.positionY}
-                           offset={this.findCommit(ref.commit).offset}
+                           offset={this.calculateCommitYOffset(ref.commit)}
             />
         );
     }
 
-    calculateCommitYPositionWithOffset(index) {
-        const {dimensions} = this.props;
+    calculateCommitYOffset(index) {
+        const {distanceY} = this.props.dimensions.commit;
         const commit = this.findCommit(index);
-        const offset = commit.offset ? commit.offset * 20 + 10 : 0;
-        return commit.level * dimensions.commit.distanceY + offset;
-    }
-
-    calculateCommitYPosition(index) {
-        const {dimensions} = this.props;
-        return this.findCommit(index).level * dimensions.commit.distanceY;
+        const refOffset = commit.offset ? commit.offset * 20 + 10 : 0;
+        const levelOffset = commit.level * distanceY;
+        const offsetDiff = distanceY - refOffset - 60;
+        return offsetDiff > 0 ? levelOffset : levelOffset - offsetDiff;
     }
 
     calculateCommitXPosition(index) {
@@ -82,12 +68,10 @@ class SVGDisplay extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <svg className='svg' ref={ref => (this.parent = ref)}>
-                    <g ref={ref => (this.child = ref)}>
+                <svg className='svg'>
                         {this.renderCommits()}
                         {this.renderArrows()}
                         {this.renderRefs()}
-                    </g>
                 </svg>
             </React.Fragment>);
     }
@@ -102,10 +86,4 @@ const mapStateToProps = (state) => {
     }
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        resizeContent: () => dispatch(resizeSvgContent(actions.RESIZE_SVG_CONTENT, true)),
-    }
-};
-
-export const SVGDisplayArea = connect(mapStateToProps, mapDispatchToProps)(SVGDisplay);
+export const SVGDisplayArea = connect(mapStateToProps)(SVGDisplay);
