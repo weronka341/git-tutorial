dcaf0d15b9a83cca5010c08f7e71114b1305206e
Weronika Szpytma <weronka341@gmail.com>
2019-12-04 20:38:13 +0100

    Refactor bottom bar component

--- src/containers/BottomBarWrapper.js
+++ src/containers/BottomBarWrapper.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Segment, Sidebar} from 'semantic-ui-react';
 import {CollapsingBar} from '../components/collapsingBar/CollapsingBar';
 import {CommandLine} from '../components/commandLine/CommandLine';
-import {setVisibility} from '../actions/ActionCreator';
-import {actions} from '../actions/Action';
-import {connect} from 'react-redux';
 
-export const BottomBar = (props) => {
+export const BottomBarWrapper = () => {
     return (
         <Sidebar
             as={Segment}
@@ -24,22 +20,3 @@ export const BottomBar = (props) => {
         </Sidebar>
     );
 };
-
-BottomBar.propTypes = {
-    isVisible: PropTypes.bool,
-    onClick: PropTypes.func,
-};
-
-const mapStateToProps = (state) => {
-    return {
-        isVisible: state.visibility.bottomBarVisible
-    }
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onClick: (isVisible) => dispatch(setVisibility(actions.SET_BOTTOMBAR_VISIBILITY, isVisible)),
-    }
-};
-
-export const BottomBarWrapper = connect(mapStateToProps, mapDispatchToProps)(BottomBar);
