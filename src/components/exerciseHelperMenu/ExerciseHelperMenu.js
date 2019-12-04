3c4a67f273d91adc085a954d6635ecd995af3aa6
Weronika Szpytma <weronka341@gmail.com>
2019-12-04 20:33:06 +0100

    Add exercise helper menu with handling events

--- src/components/exerciseHelperMenu/ExerciseHelperMenu.js
+++ src/components/exerciseHelperMenu/ExerciseHelperMenu.js
@@ -1,48 +1,54 @@
 import React from 'react';
-import {Icon, Menu, Message, Modal, Popup} from 'semantic-ui-react';
-import {moveToContent, moveToExercise, setVisibility} from '../../actions/ActionCreator';
 import {connect} from 'react-redux';
+import {Card, Header, Icon, Menu, Modal, Popup} from 'semantic-ui-react';
+import {moveToContent, moveToExercise, performActionOnSidebarOption, setVisibility} from '../../actions/ActionCreator';
 import {Tip} from '../tip/Tip';
 import {exercisesText} from '../../content/tutorial/ExercisesText';
 import {actions} from '../../actions/Action';
+import './ExerciseHelperMenu.css';
+import {selectNextOption} from '../../containers/tutorialContentContainer/TutorialContentContainer';
 
-export const ExerciseHelper = (props) => (
-    <Menu compact>
-        <Menu.Item name='redo'>
-            <Popup content='Zacznij od nowa'
-                   position='top right'
-                   trigger={
-                       <Icon name='redo'
-                             onClick={() => props.moveToExercise(props.activeExercise)}
-                       />
-                   }
-            />
-        </Menu.Item>
+export const ExerciseHelper = (props) => {
+    return (
+        <Menu compact>
+            <Menu.Item name='redo'>
+                <Popup content='Zacznij od nowa'
+                       position='top right'
+                       trigger={
+                           <Icon name='redo'
+                                 onClick={() => props.moveToExercise(props.activeExercise)}
+                           />
+                       }
+                />
+            </Menu.Item>
 
-        <Modal trigger={renderTipPopup()}>
-            <Tip title='Wskazówka'>
-                {exercisesText[props.activeExercise].tip}
-            </Tip>
-        </Modal>
+            <Modal trigger={renderTipPopup()}>
+                <Tip title='Wskazówka'>
+                    {exercisesText[props.activeExercise].tip}
+                </Tip>
+            </Modal>
 
-        <Menu.Item name='close'>
-            <Popup content='Zakończ ćwiczenie'
-                   position='top right'
-                   trigger={
-                       <Icon name='close'
-                             onClick={() => handleExitExercise(props)}
-                       />
-                   }
-            />
-        </Menu.Item>
-        <Modal open={props.isExerciseCompleted}
-               onClose={() => handleExitExercise(props)}>
-            <Message positive>
-                <Message.Header>Brawo! Pomyślnie zaliczyłeś ćwiczenie!</Message.Header>
-            </Message>
-        </Modal>
-    </Menu>
-);
+            <Menu.Item name='close'>
+                <Popup content='Zakończ ćwiczenie'
+                       position='top right'
+                       trigger={
+                           <Icon name='close'
+                                 onClick={() => handleExitExercise(props)}
+                           />
+                       }
+                />
+            </Menu.Item>
+            <Modal id='task-completed-modal'
+                   className='show-in'
+                   open={props.isExerciseCompleted}
+                   onClose={() => {
+                       handleCompleteExercise(props)
+                   }}>
+                {renderCompletedTaskInfo()}
+            </Modal>
+        </Menu>
+    );
+};
 
 const renderTipPopup = () => (
     <Menu.Item name='tip'>
@@ -53,14 +59,42 @@ const renderTipPopup = () => (
     </Menu.Item>
 );
 
+const renderCompletedTaskInfo = () => (
+    <Card fluid color='teal'>
+        <Card.Content className='teal'>
+            <Header as='h3'>
+                <Icon name='check'/>
+                <Header.Content>
+                    Zadanie wykonane
+                </Header.Content>
+            </Header>
+        </Card.Content>
+        <Card.Content>
+            <Card.Description>
+                Brawo! Pomyślnie zaliczyłeś ćwiczenie!
+            </Card.Description>
+        </Card.Content>
+    </Card>
+);
+
 const handleExitExercise = (props) => {
     props.moveToContent();
     props.hideConsole();
     props.showSidebar();
 };
 
+const handleCompleteExercise = (props) => {
+    props.moveToContent();
+    props.addToVisited(props.activeOption);
+    selectNextOption(props);
+    props.hideConsole();
+    props.showSidebar();
+};
+
 const mapStateToProps = (state) => {
     return {
+        activeOption: state.options.activeOption,
+        visitedOptions: state.options.visitedOptions,
         activeExercise: state.animations.activeExercise,
         isExerciseCompleted: state.animations.isExerciseCompleted,
     }
@@ -72,6 +106,8 @@ const mapDispatchToProps = (dispatch) => {
         moveToContent: () => dispatch(moveToContent()),
         showSidebar: () => dispatch(setVisibility(actions.SET_SIDEBAR_VISIBILITY, true)),
         hideConsole: () => dispatch(setVisibility(actions.SET_BOTTOMBAR_VISIBILITY, false)),
+        setActive: (option) => dispatch(performActionOnSidebarOption(actions.SET_ACTIVE_SIDEBAR_OPTION, option)),
+        addToVisited: (option) => dispatch(performActionOnSidebarOption(actions.ADD_OPTION_TO_VISITED, option))
     }
 };
 
